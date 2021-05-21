2dafd4b597b34f81d1d25dba9c6ae9a08a5944f6
wp <82819675+suvorov19@users.noreply.github.com>
2021-05-21 21:45:49 +0300

    Updated PlayersSeparator.m with additional information

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,27 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
-}
 
+    int count = 0;
+    
+    for (int i = 0; i < (int) ratingArray.count; i++) {
+        for (int j = i; j < (int) ratingArray.count; j++) {
+            for (int k = j; k < (int) ratingArray.count; k++) {
+                if ([ratingArray[i] intValue] > [ratingArray[j] intValue] && [ratingArray[j] intValue] > [ratingArray[k] intValue]) {
+                    count++;
+                }
+            }
+        }
+    }
+    for (int i = (int)ratingArray.count - 1; i >= 0; i--) {
+        for (int j = i; j >= 0; j--) {
+            for (int k = j; k >= 0; k--) {
+                if ([ratingArray[i] intValue] > [ratingArray[j] intValue] && [ratingArray[j] intValue] > [ratingArray[k] intValue]) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
 @end
