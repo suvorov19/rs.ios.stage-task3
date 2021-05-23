8eb9e4a9bd168ef6aaedd79fdd2db6e316daab55
wp <82819675+suvorov19@users.noreply.github.com>
2021-05-23 16:01:51 +0300

    Update LexicographicallyMinimal.m with additional information

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,30 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *string1Mutable = [string1 mutableCopy];
+    NSMutableString *string2Mutable = [string2 mutableCopy];
+    NSMutableString *resultString = [NSMutableString string];
+    
+    while ((int)string1Mutable.length  + (int)string2Mutable.length  != 0) {
+        
+        if (string1Mutable.UTF8String[0] <= string2Mutable.UTF8String[0]) {
+            [resultString appendString:[string1Mutable substringWithRange:NSMakeRange(0,1)]];
+            [string1Mutable deleteCharactersInRange:NSMakeRange(0,1)];
+        }
+        else if (string1Mutable.UTF8String[0] >= string2Mutable.UTF8String[0]) {
+            [resultString appendString:[string2Mutable substringWithRange:NSMakeRange(0,1)]];
+            [string2Mutable deleteCharactersInRange:NSMakeRange(0,1)];
+        }
+        if ((int)string1Mutable.length == 0) {
+            [resultString appendString:string2Mutable];
+            [string2Mutable deleteCharactersInRange:NSMakeRange(0, string2Mutable.length)];
+        }
+        else if ((int)string2Mutable.length == 0) {
+            [resultString appendString:string1Mutable];
+            [string1Mutable deleteCharactersInRange:NSMakeRange(0, string1Mutable.length)];
+        }
+    }
+    return resultString;
 }
-
 @end
